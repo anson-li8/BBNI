dea32e569d80c18f87c72a0e98c0310f9956efde
Anson Li <liyuanrui618@gmail.com>
2026-06-26 13:33:26 -0500

    clean up ProposalConstruction and ConstructInitial functions for clarity and efficiency

--- R/proposals.R
+++ R/proposals.R
@@ -12,7 +12,6 @@
 #' @noRd
 ProposalConstruction <- function(GeneData, SampleSize) {
   gene.data <- GeneData
-  num.node <- nrow(gene.data)
   error.prop <- 0.4
   pseudo.count <- 0.01
   SampleSize <- SampleSize + pseudo.count * 8
@@ -23,8 +22,8 @@ ProposalConstruction <- function(GeneData, SampleSize) {
     for (j in 1:nrow(gene.data)) {
       if (j != i) {
         for (k in 1:nrow(gene.data)) {
-          if (k != i && k != j) { # print(i)
-            test.result <- list() # save test results for all possible relations
+          if (k != i && k != j) {
+            test.result <- list()
             gene.triplet <- rbind(rbind(gene.data[i, 1:(SampleSize - 1)], gene.data[j, 1:(SampleSize - 1)]), gene.data[k, 2:SampleSize])
             c000 <- 0
             c001 <- 0
@@ -61,7 +60,7 @@ ProposalConstruction <- function(GeneData, SampleSize) {
                 c111 <- c111 + 1
               }
             }
-            test.stat <- c(c000, c001, c010, c011, c100, c101, c110, c111) #  generate random sample
+            test.stat <- c(c000, c001, c010, c011, c100, c101, c110, c111)
             test.result[[1]] <- c(i, j, k, 1, BF1(test.stat, pseudo.count, SampleSize, threshold))
             test.result[[2]] <- c(i, j, k, 2, BF2(test.stat, pseudo.count, SampleSize, threshold))
             test.result[[3]] <- c(i, j, k, 3, BF3(test.stat, pseudo.count, SampleSize, threshold))
@@ -76,8 +75,6 @@ ProposalConstruction <- function(GeneData, SampleSize) {
             test.result[[12]] <- c(i, j, k, 12, BF12(test.stat, pseudo.count, SampleSize, threshold)) # model g_k=g_j
             test.result[[13]] <- c(i, j, k, 13, BF13(test.stat, pseudo.count, SampleSize, threshold)) # model g_k=complement(g_i)
             test.result[[14]] <- c(i, j, k, 14, BF14(test.stat, pseudo.count, SampleSize, threshold)) # model g_k=complement(g_j)
-            # save the most likely of all 14 relations by their false counts.
-            # *******************here it may filter out some true ones due to noise and model uncertainty ****************
             miscount <- numeric()
             jj <- 1
             for (ii in 1:length(test.result)) {
@@ -95,7 +92,7 @@ ProposalConstruction <- function(GeneData, SampleSize) {
                   kk <- kk + 1
                 }
               }
-            } # for each pariwise genes or gene triplet, only the most likely one is output
+            }
           }
         }
       }
@@ -106,7 +103,6 @@ ProposalConstruction <- function(GeneData, SampleSize) {
     candidate[i, ] <- candidate.prior[[i]]
   }
   order.candidate <- candidate[order(candidate[, 3]), ] # order by output variables
-  ##################################   # transform BIC into proportion
   num.node <- max(order.candidate[, 3])
   triplet <- list()
   j1 <- 1
@@ -156,16 +152,15 @@ ProposalConstruction <- function(GeneData, SampleSize) {
   weighted.pairwise <- cbind(unique.pairwise, unique.pairwise[, 4])
   constant <- 0.001
   for (i in 1:num.node) {
-    if (i %in% candidate.triplet[, 3] == T) {
+    if (i %in% candidate.triplet[, 3]) {
       for (j in 1:nrow(candidate.triplet)) {
         if (candidate.triplet[j, 3] == i) {
-          trip.matrix <- matrix(ncol = 5)
           trip.matrix <- candidate.triplet[candidate.triplet[, 3] == i, 1:5]
-          trip.matrix <- data.matrix(trip.matrix) # avoid pair.matrix to be "numeric"
+          trip.matrix <- data.matrix(trip.matrix)
           if (ncol(trip.matrix) == 1) {
             trip.matrix <- t(trip.matrix)
           }
-          score <- 1 / trip.matrix[, 5] # use reciprocal of miscunt as weight
+          score <- 1 / trip.matrix[, 5]
           prop <- (score + constant) / (sum(score + constant))
           trip.matrix[, 5] <- prop
           weighted.triplet[weighted.triplet[, 3] == i, ] <- cbind(trip.matrix, weighted.triplet[weighted.triplet[, 3] == i, 6])
@@ -175,16 +170,15 @@ ProposalConstruction <- function(GeneData, SampleSize) {
   }
 
   for (i in 1:num.node) {
-    if (i %in% unique.pairwise[, 2] == T) {
+    if (i %in% unique.pairwise[, 2]) {
       for (j in 1:nrow(unique.pairwise)) {
         if (unique.pairwise[j, 2] == i) {
-          pair.matrix <- matrix(ncol = 4)
           pair.matrix <- unique.pairwise[unique.pairwise[, 2] == i, 1:4]
-          pair.matrix <- data.matrix(pair.matrix) # avoid pair.matrix to be "numeric"
+          pair.matrix <- data.matrix(pair.matrix)
           if (ncol(pair.matrix) == 1) {
             pair.matrix <- t(pair.matrix)
           }
-          score <- 1 / (pair.matrix[, 4] + constant) # note here
+          score <- 1 / (pair.matrix[, 4] + constant)
           prop <- score / (sum(score))
           pair.matrix[, 4] <- prop
           weighted.pairwise[weighted.pairwise[, 2] == i, ] <- cbind(pair.matrix, weighted.pairwise[weighted.pairwise[, 2] == i, 5])
@@ -220,40 +214,27 @@ ConstructInitial <- function(Candidate, num.node) {
   trans_func_matrix <- matrix(0, nrow = num.node, ncol = num.node)
   prop_trans_func_matrix <- matrix(0, nrow = num.node, ncol = num.node)
   prop_incid_matrix <- matrix(0, nrow = num.node, ncol = num.node)
-  prop_ances_matrix <- matrix(0, nrow = num.node, ncol = num.node)
   incid_matrix <- matrix(0, nrow = num.node, ncol = num.node)
   ratio <- 0.5
   for (i in 1:num.node)
   {
     prop_trans_func_matrix <- trans_func_matrix
     prop_incid_matrix <- incid_matrix
-    #################################
-    pairwise.prior.set <- matrix() # clear variables     # determine candidate parents for  node i
-    pairwise.prior.pare <- matrix()
+    pairwise.prior.set <- matrix()
     pairwise.prior.set <- prior.pairwise[prior.pairwise[, 2] == i, ] # priors for node update_order[k]
     pairwise.prior.set <- data.matrix(pairwise.prior.set)
     if (ncol(pairwise.prior.set) == 1) {
       pairwise.prior.set <- t(pairwise.prior.set)
-      pairwise.prior.pare <- t(data.matrix(pairwise.prior.set[, 1]))
-    }
-    if (ncol(pairwise.prior.set) > 1 && nrow(pairwise.prior.set) > 0) {
-      pairwise.prior.pare <- pairwise.prior.set[, 1]
     }
 
     triplet.prior.set <- matrix()
-    triplet.prior.pare <- matrix()
     triplet.prior.set <- prior.triplet[prior.triplet[, 3] == i, ]
     triplet.prior.set <- data.matrix(triplet.prior.set)
-    if (ncol(triplet.prior.set) > 1 && nrow(triplet.prior.set) > 0) {
-      triplet.prior.pare <- triplet.prior.set[, 1:2]
-    }
     if (ncol(triplet.prior.set) == 1) {
       triplet.prior.set <- t(triplet.prior.set)
-      triplet.prior.pare <- t(data.matrix(triplet.prior.set[, 1:2]))
     }
     prop_prob <- runif(1)
     aa <- 0
-    ##############################################
     if (prop_prob >= ratio) # consider two parents
       {
         if (nrow(triplet.prior.set) >= 1) {
@@ -275,10 +256,10 @@ ConstructInitial <- function(Candidate, num.node) {
             }
 
             max.score.candidate <- bbb[bbb[, 5] == max(score), ]
-            if (is.numeric(max.score.candidate) == T) {
+            if (is.numeric(max.score.candidate)) {
               add_parent <- max.score.candidate
             }
-            if (is.matrix(max.score.candidate) == T) {
+            if (is.matrix(max.score.candidate)) {
               if (nrow(max.score.candidate) > 1) {
                 add_parent <- max.score.candidate[sample.int(nrow(max.score.candidate), 1), ]
               }
@@ -305,17 +286,16 @@ ConstructInitial <- function(Candidate, num.node) {
           }
         }
       }
-    ##########################################
     if (prop_prob < ratio) { # consider one parent
       if (nrow(pairwise.prior.set) > 0) {
         candidate.pare.set <- pairwise.prior.set
-        if (is.numeric(candidate.pare.set) == T) {
+        if (is.numeric(candidate.pare.set)) {
           add_parent <- candidate.pare.set
         }
-        if (is.matrix(candidate.pare.set) == T) {
+        if (is.matrix(candidate.pare.set)) {
           add_parent <- candidate.pare.set[candidate.pare.set[, 4] == max(candidate.pare.set[, 4]), ]
         }
-        if (is.matrix(add_parent) == T) {
+        if (is.matrix(add_parent)) {
           if (nrow(add_parent) > 1) {
             add_parent <- add_parent[sample.int(nrow(add_parent), 1), ]
           }
@@ -357,7 +337,7 @@ ConstructInitial <- function(Candidate, num.node) {
 #'
 #' @return A transition-function matrix with the same dimensions as the input. A 0 indicates no incoming edge. Non-zero entries contain a code from 1-12 identifying which Boolean operation each gene utilizes for calculation of the input of its parent(s).
 #' @noRd
-Prop_Trans_Func_Matrix <- function(prop_incid_matrix) # based on incidence matrix, define transition function matrix
+Prop_Trans_Func_Matrix <- function(prop_incid_matrix)
 {
   prop_trans_func_matrix <- prop_incid_matrix
   jj <- numeric()
